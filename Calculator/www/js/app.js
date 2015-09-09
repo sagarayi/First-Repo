27cf3edbae1acba9e5ae990b67b94c42f7adad7d
Sagar Ayi <ayisagar@gmail.com>
2015-09-09 14:04:34 +0530

    Initial logic.

--- Calculator/www/js/app.js
+++ Calculator/www/js/app.js
@@ -17,3 +17,130 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+var op1=0;
+var op2=null;
+var sym=null;
+var done=0,switched=0,sign=0;
+function calculator(input)
+{
+  if(done==1)
+  {
+    cls();
+    display();
+  }
+  switch(parseInt(input))
+  {
+    case -2:cls();
+            break;
+    case -1:bs();
+            break;
+    case 0:;
+    case 1:;
+    case 2:;
+    case 3:;
+    case 4:;
+    case 5:;
+    case 6:;
+    case 7:;
+    case 8:;
+    case 9:digit(input);
+            break;
+    case 11:;
+    case 12:;
+    case 13:;
+    case 14:setSym(input);
+            break;
+    case 15:arith();
+            //cls();
+            break;
+    }
+    
+  }
+  function cls()
+  {
+    op1=0;
+    op2=null;
+    sym=null;
+    done=0;
+    switched=0;
+    display();
+  }
+  function bs()
+  {
+   var res;
+    if(switched==0)
+    {
+        res=document.getElementById("exp1").innerHTML;
+        res=parseInt(res/10);
+        op1=res;
+        display();
+    }
+    else
+    {
+      res=document.getElementById("exp3").innerHTML; 
+      res=parseInt(res/10);
+      op2=res;
+      display();
+    } 
+  }
+  function digit(i)
+  {
+    var res;
+    if(switched==0)
+    {
+        res=document.getElementById("exp1").innerHTML;
+        res=res*10+parseInt(i);
+        op1=res;
+        display();
+    }
+    else
+    {
+      res=document.getElementById("exp3").innerHTML; 
+      res=res*10+parseInt(i);
+      op2=res;
+      display();
+    }
+
+  }
+  function setSym(i)
+  {
+    switch(parseInt(i))
+    {
+      case 11:sym="X";
+              break;
+      case 12:sym=" + ";
+              break;
+      case 13:sym=" - ";
+              break;
+      case 14:sym=" / ";
+              break;
+    }
+    sign=parseInt(i);
+    switched=1;
+    display();
+  }
+  function arith()
+  {var res;
+    switch(sign)
+    {
+      case 11:res=op1*op2;
+              break;
+      case 12:res=op1+op2;
+              break;
+      case 13:res=op1-op2;
+              break;
+      case 14:res=op1/op2;
+              break;
+    }
+    op1=res;
+    op2=null;
+    sym=null;
+    done=1;
+    display();
+  }
+  function display()
+  {
+    document.getElementById("exp1").innerHTML=op1;
+    document.getElementById("exp2").innerHTML=sym;
+    document.getElementById("exp3").innerHTML=op2;
+  }
